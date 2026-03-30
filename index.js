ab9569bc36ccdda068a28fa45f9c16eb98f2f0f2
mustafiz82 <mustafiz8260@gmail.com>
2026-03-30 15:10:38 +0600

    latest update

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
+const dns = require("dns")
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 const express = require('express')
-const { MongoClient, ServerApiVersion, Db } = require('mongodb');
+const { MongoClient, ServerApiVersion, Db, ObjectId } = require('mongodb');
+const cors = require('cors')
 const app = express()
 const port = 3000
-const uri = "mongodb+srv://mustafiz8260_db_user:tcqlEbzee0oFm8F3@cluster0.llozej9.mongodb.net/?appName=Cluster0";
+// const uri = "mongodb+srv://mustafiz8260_db_user:tcqlEbzee0oFm8F3@cluster0.llozej9.mongodb.net/?appName=Cluster0";
+const uri = "mongodb+srv://mustafiz8260_db_user:8tq57oekMUGifUYG@cluster0.jq9emig.mongodb.net/?appName=Cluster0";
 
 
 app.use(express.json())
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send({ message: `Server Is Running on Port , ${port}` })
 })
 
+// 8tq57oekMUGifUYG
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,10 +42,21 @@ async function run() {
 
         const DB = client.db("Furniro")
         const userCollection = DB.collection("user")
+        const productCollection = DB.collection("product")
+        const categoryCollection = DB.collection("category")
 
 
       app.post("/user" , async (req , res )  => {
         const data = req.body
+
+        const email = data.email
+
+        const userEsixt = await userCollection.findOne({email : email})
+        console.log(userEsixt);
+
+        if(userEsixt){
+            return res.send({message : "user already exist"})
+        }
 
         console.log(data);
 
@@ -50,6 +68,46 @@ async function run() {
 
       app.get("/user" , async (req , res) => {
         const result = await userCollection.find().toArray()
+        res.send(result)
+      })
+
+      app.post("/category" , async (req , res) => {
+        try {
+            const data = req.body
+            const response = await categoryCollection.insertOne(data)
+            res.status(201).send(response)
+        } catch (error) {
+            res.status(400).send({message : error.message})
+        }
+      })
+
+
+      app.get("/category" , async (req  , res) => {
+        try {
+
+            const respose = await categoryCollection.find().toArray()
+            res.send(respose)
+            
+        } catch (error) {
+            res.status(400).send({message : error.message})
+        }
+    })
+    
+    app.delete("/category/:id" , async (req , res) => {
+        try {
+            const id = req.params.id
+            const response = await categoryCollection.deleteOne({_id : new ObjectId(id)})
+            res.send(response)
+        } catch (error) {
+            
+            res.status(400).send({message : error.message})
+        }
+      })
+
+
+      app.post("/product" , async (req , res) => {
+        const data = req.body
+        const result = await productCollection.insertOne(data)
         res.send(result)
       })
 
